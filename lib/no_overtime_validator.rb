fc66345d7ff3f185ed52bc2c4e978a4560c82eac
dray <dray@westmont.edu>
2012-11-07 23:46:09 -0800

    fix bug with no overtime validation

--- lib/no_overtime_validator.rb
+++ lib/no_overtime_validator.rb
@@ -1,7 +1,7 @@
 class NoOvertimeValidator < ActiveModel::Validator
   def validate(record)
-    if record.end - record.start > 8 
-      record.errors[:base] << "Shifts can't be longer than eight hours"
+    if record.end - record.start > 8*60*60 # Time in seconds 
+      record.errors[:base] << "Shifts can't be longer than eight hours."
     end
   end
 end
